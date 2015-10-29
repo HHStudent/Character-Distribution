4ae687d00982b14c044b228b5b50c9308c47d162
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-10-29 14:50:51 -0400

    Updated from Brython Server: 10/29/2015 2:50:51 PM

--- distribution.py
+++ distribution.py
@@ -36,3 +36,14 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+#s = str(input("Please enter a string of text (the bigger the better): "))
+s = ("The rain in Spain stays mainly in the plain.")
+
+print('The distribution of characters in "{0}" is:'.format(s))
+
+s1 = s.lower().strip('.')
+
+print(s1)
+
+
+
