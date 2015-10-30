47f63004ef82f8b2d984b3018067938e45f41998
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-10-30 14:20:06 -0400

    Updated from Brython Server: 10/30/2015 2:20:15 PM

--- distribution.py
+++ distribution.py
@@ -36,20 +36,22 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-import string
 #s = str(input("Please enter a string of text (the bigger the better): "))
 s = ("The rain in Spain stays mainly in the plain.")
 
 print('The distribution of characters in "{0}" is:'.format(s))
 
-alphabet = list(string.ascii_lowercase)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 s1 = s.lower().strip('.')
+list1 = []
+var1 = 0
 
 for x in alphabet:
-    num = s1.count(x)
-    num1 = list(string.num)
+    n1 = s1.count(x)
+    
 
-print(''.join(sorted(num1)))
+
+
 
 
 
